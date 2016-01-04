8ac0da636ecc250ae6c9bbc020a2e01056b8700d
daschaich <dschaich@syr.edu>
2016-01-04 13:41:06 -0500

    Initial four-fermion condensate measurement

--- RHMC/correlators.c
+++ RHMC/correlators.c
@@ -64,10 +64,20 @@ void vol_src() {
 // Measure two- and four-fermion correlators
 // Return total number of iterations
 int correlators(int *pnt) {
-  int i, iters, tot_iters = 0, sav = Norder;
-  Real size_r;
+  register int i, j, k, l;
+  register site *s;
+  int index, iters, tot_iters = 0, sav = Norder;
+  Real size_r, four = 0.0, ***f;
   double dtime;
   vector **psim;
+
+  // Allocate structure to hold all DIMF propagators
+  f = malloc(DIMF * sizeof(***f));
+  for (i = 0; i < DIMF; i++) {
+    f[i] = malloc(DIMF * sizeof(vector*));
+    for (j = 0; j < DIMF; j++)
+      f[i][j] = malloc(sites_on_node * sizeof(vector));
+  }
 
   // Hack a basic CG out of the multi-mass CG
   Norder = 1;
@@ -75,21 +85,59 @@ int correlators(int *pnt) {
   psim[0] = malloc(sites_on_node * sizeof(vector));
   shift[0] = 0;
 
-  for (i = 0; i < DIMF; i++) {
+  for (j = 0; j < DIMF; j++) {
     dtime = -dclock();
-    pnt_src(pnt, i);
+    pnt_src(pnt, j);
     iters = congrad_multi(src, psim, niter, rsqmin, &size_r);
     dtime += dclock();
     tot_iters += iters;
     node0_printf("Inversion %d of %d took %d iters and %.4g seconds\n",
-                 i + 1, DIMF, iters, dtime);
+                 j + 1, DIMF, iters, dtime);
+
+    // Copy psim into f[j][k]
+    FORALLSITES(i, s) {
+      for (k = 0; k < DIMF; k++)
+        f[j][k][i] = psim[0][i].c[k];
+    }
 
     // Now construct correlators
     // TODO: ...
   }
 
+  // Compute four-fermion condensate
+  if (node_number(pnt[0] % nx, pnt[1] % ny, pnt[2] % nt) == mynode()) {
+    index = node_index(pnt[0] % nx, pnt[1] % ny, pnt[2] % nt);
+    for (i = 0; i < DIMF; i++) {
+      for (j = 0; j < DIMF; j++) {
+        if (j == i)
+          continue;
+        for (k = 0; k < DIMF; k++) {
+          if (k == j || k == i)
+            continue;
+          for (l = 0; l < DIMF; l++) {
+            if (l == k || l == j || l == i)
+              continue;
+          four += perm[i][j][k][l] * f[i][j][index] * f[k][l][index];
+          }
+        }
+      }
+    }
+  }
+  g_doublesum(&four);
+
   // Normalize correlators and print results
   // TODO: ...
+
+  node0_printf("FOUR %d %d %d %.6g %d\n",
+               pnt[0] % nx, pnt[1] % ny, pnt[2] % nt, four, tot_iters);
+
+  // Free structure to hold all DIMF propagators
+  for (i = 0; i < DIMF; i++) {
+    for (j = 0; j < DIMF; j++)
+      free(f[i][j]);
+    free(f[i]);
+  }
+  free(f);
 
   // Reset multi-mass CG and clean up
   Norder = sav;
